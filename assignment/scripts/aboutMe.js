a24e72c182590b0462bbb970ba8a7ae4f98e484a
Tony LaForgia <alforgia428@gmail.com>
2021-12-24 16:29:28 -0500

    Completed Stretch Goal Q# 18

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -91,3 +91,5 @@ switch (mostPets) {
     console.log(mostPets);
 }
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+adventurous = adventurous ? 'Adventures are great!' : 'How about we stay home?'; 
+console.log(adventurous);
